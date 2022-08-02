5bb50cde0035a0d51b2f4335a4f09710685e54ca
ckk3 <contato.gustavomota@gmil.com>
2022-08-01 21:41:53 -0300

    importando database

--- cap12/helloword.R
+++ cap12/helloword.R
@@ -1,1 +1,10 @@
-print("Hello World!")
+# setando o diretorio do arquivo
+setwd("E:/DEV/powerbi-datascienceacademy/cap12")
+
+#Importando dataset
+vendas <- read.csv('vendas.csv', fileEncoding = "windows-1252")
+
+#Resumo do dataset
+View(vendas)
+str(vendas)
+summary(vendas$Valor)
