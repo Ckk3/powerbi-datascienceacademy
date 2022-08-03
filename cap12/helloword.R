6287f9a4cd1b3cabd5a295ec141c4d8454853152
ckk3 <contato.gustavomota@gmil.com>
2022-08-02 23:52:10 -0300

    outras estatisticas com E

--- cap12/helloword.R
+++ cap12/helloword.R
@@ -8,3 +8,63 @@ vendas <- read.csv('vendas.csv', fileEncoding = "windows-1252")
 View(vendas)
 str(vendas)
 summary(vendas$Valor)
+
+#Media Aritimetica
+mean(vendas$Valor)
+
+#Media Ponderada
+weighted.mean(vendas$Valor, w = vendas$Custo)
+
+#mediana
+median(vendas$Valor)
+
+#Moda
+# Create the function.
+moda <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+#ver resultado
+resultado <- moda(vendas$Valor)
+print(resultado)
+
+modacusto <- moda(vendas$Custo)
+print(modacusto)
+
+
+#Criando grafico de media
+install.packages("ggplot2")
+library(ggplot2)
+
+#Criar grafico
+ggplot(vendas) + 
+  stat_summary(aes(x = Estado, y = Valor), fun = mean, geom = "bar") + 
+  labs(title = "Média de Valor Por Estado ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
